fcb1b8bd36e958873eed3c8f041d06c30a0fa3c5
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-10-18 18:38:32 +0500

    7 misol

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,7 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
+from cmath import pi
+def main(a):
+    return a
+print(main(pi))
